f0a47fae33d7de6370b041b8ad63105a2fbe9e8a
Peter Bieringer <pb@bieringer.de>
2022-07-20 07:39:41 +0200

    update version

--- tvinfomerk2vdr-ng.pl
+++ tvinfomerk2vdr-ng.pl
@@ -56,7 +56,7 @@ binmode(STDOUT, ":utf8");
 binmode(STDERR, ":utf8");
 
 our $progname = "tvinfomerk2vdr-ng.pl";
-our $progversion = "1.0.3";
+our $progversion = "1.1.0";
 
 ## Requirements:
 # Ubuntu: libxml-simple-perl libdate-calc-perl
